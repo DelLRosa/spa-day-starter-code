b6870a396a19da54f5377e2fe1263928f267a99b
Delaney <delaney.rosa1@gmail.com>
2023-05-08 19:10:58 -0500

    Completed vanilla studio

--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -1,9 +1,21 @@
 package org.launchcode.spaday.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 public class User {
+    @NotBlank
+    @Size(min=5, max=15, message="Username must be between 5 and 15 characters")
     private String username;
+    @Email(message = "Please enter a valid email address.")
     private String email;
+    @NotBlank(message = "please enter a password.")
+    @Size(min=6, message = "Password must be at least 6 characters long.")
     private String password;
+
+    @NotBlank(message = "please verify your password.")
+    private String verify;
 
     public User() {
 
@@ -38,5 +50,13 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getVerify() {
+        return verify;
+    }
+
+    public void setVerify(String verify) {
+        this.verify = verify;
     }
 }
